0d3f7d0435f3801399a4afaebaac002cea1d6e87
ibnAbdelkader <ahmedqad7790@gmail.com>
2024-08-15 15:42:42 +0300

    upload

---
+++ AVR_SPI/KeypadLCD/MC1.c
@@ -0,0 +1,21 @@
+﻿/*
+ * MC1.c
+ *
+ * Created: 25/04/2015 11:30:09 م
+ *  Author: Ahmed Abdelkader
+ */ 
+
+#include "Keypad.h"
+#include "SPI.h"
+
+int main(void)
+{
+	uint8 key;
+	SPI_initMaster(); //intitialize Master MC
+    while(1)
+    {
+		key = KeyPad_getPressedKey(); //get the pressed key
+		SPI_sendByte(key); //send the pressed key to the second MC using spi
+		_delay_ms(500); //press time is 500msec
+    }
+}
---
+++ AVR_SPI/KeypadLCD/MC2.c
@@ -0,0 +1,28 @@
+﻿/*
+ * MC2.c
+ *
+ * Created: 25/04/2015 11:32:23 م
+ *  Author: Ahmed Abdelkader
+ */ 
+
+#include "SPI.h"
+#include "LCD.h"
+
+int main(void)
+{
+	uint8 key;
+	LCD_init();
+	SPI_initSlave(); //initialize salve MC
+    while(1)
+    {
+		key = SPI_recieveByte();  //receive the pressed key from spi
+		if((key>=0) && (key<=9))
+		{
+			LCD_intgerToString(key); //display the pressed key
+		}
+		else
+		{
+			LCD_displayCharacter(key);
+		}	
+    }
+}
